30f82431e153056015f7f880d6e9bb0c90227f98
rishiirajanand <rishirajanand5@gmail.com>
2024-03-23 11:57:46 +0530

    print hello world

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const reactHeading = React.createElement('h1', {className:'head',id:'head'}, 'Hello React!!');
+
+ReactDOM.createRoot(document.getElementById('root')).render(reactHeading);
+
+console.log(reactHeading);
